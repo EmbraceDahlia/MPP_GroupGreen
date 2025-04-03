92431ff314332114de55ae8905f1edff752abb85
Dahlia <embracedahlia@gmail.com>
2025-04-03 09:13:48 -0500

    Exercise 4 diagram and code solution

--- lab3/Exercise4/House.java
+++ lab3/Exercise4/House.java
@@ -1,16 +1,15 @@
 package lab3.Exercise4;
 
-public class House  {
+public class House extends Property {
 
-	private double lotSize;
-	
-	
-	public House(double lotSize) {
-		this.lotSize = lotSize;
-	}
+    private double lotSize;
 
+    public House(double lotSize) {
+        this.lotSize = lotSize;
+    }
 
-	public double computeRent(){
-		return 0.1 * lotSize;
-	}
+    @Override
+    public double computeRent() {
+        return 0.1 * lotSize;
+    }
 }
---
+++ lab3/Exercise4/Property.java
@@ -0,0 +1,10 @@
+package lab3.Exercise4;
+
+public class Property {
+    private int id;
+    private double rent;
+
+    public double computeRent() {
+        return rent;
+    }
+}
