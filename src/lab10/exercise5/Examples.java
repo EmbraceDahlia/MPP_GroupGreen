aaa8b3e87629b5cf0e395c2d12d9c88a0c55b784
Dahlia <embracedahlia@gmail.com>
2025-04-16 09:03:24 -0500

    Lambda and Method Reference Exercises

---
+++ src/lab10/exercise5/Examples.java
@@ -0,0 +1,10 @@
+package lab10.exercise5;
+
+public class Employee {
+    private String name;
+    private int salary;
+    public Employee(String name,int salary){
+        this.name=name;
+        this.salary=salary;
+    }
+}
